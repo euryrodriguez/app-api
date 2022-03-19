7ab20a50840309dac42a8bca53ca9d92549b5696
eury958 <eury958@gmail.com>
2022-03-18 20:17:08 -0400

    archivo de configuracion para la base de datos.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+let corsOptions  = {
+    origin: '*'
+}
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=>{
     res.json({ status: 1, message: 'it works.'});
