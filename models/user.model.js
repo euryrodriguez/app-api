2f59b9908c0b1316de7b18dd99855b29d1af4972
eury958 <eury958@gmail.com>
2022-03-18 20:27:29 -0400

    Modelo de usuarios.

---
+++ models/user.model.js
@@ -0,0 +1,15 @@
+module.exports = () =>{
+    const User = mongoose.model(
+        "usuarios",
+        mongoose.Schema(
+            {
+                _id: String,
+                createdDate: Date,
+                email: String,
+                firstName: String,
+                lastName: String,
+                password: String,
+                role: String
+            }))
+            return User;
+}
